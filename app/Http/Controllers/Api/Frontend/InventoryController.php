723c7af8c66998c0dfe9ff6bf9dec396c578da98
Jenish <jenish.rising@gmail.com>
2026-04-01 15:07:51 +0530

    changes

--- app/Http/Controllers/Api/Frontend/InventoryController.php
+++ app/Http/Controllers/Api/Frontend/InventoryController.php
@@ -185,6 +185,7 @@ class InventoryController extends Controller
 
             $machinery->name = trim("$year $make $model");
             $machinery->working_hours = $workingHours;
+            $machinery->is_purchase = (int) ($machinery->status == 2);
 
             $machinery->category = $machinery->category ? $machinery->category->category_name : null;
 
