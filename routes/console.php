ec708b6e9205d981e0950f191824c076cc54f34c
Jenish Kanpariya <amakwana96.am@gmail.com>
2026-05-23 10:15:01 +0530

    conjob issue fix

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('app:extend-expired-auctions')
+    ->everyTenMinutes()
+    ->appendOutputTo(storage_path('logs/ExpireTime.log'));
+
+Schedule::command('app:check-bid-end-time')
+    ->everyTenMinutes()
+    ->appendOutputTo(storage_path('logs/checkBidEndTime.log'));
